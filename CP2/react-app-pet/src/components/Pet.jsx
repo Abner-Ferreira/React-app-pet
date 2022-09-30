0251b9046314a5ee30652bed8d8be9460a7091e4
hitoshidevx <g.hitoshi.yokogawa@gmail.com>
2022-09-30 20:56:20 -0300

    Começando o component 'Pet'

---
+++ CP2/react-app-pet/src/components/Pet.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Pet() {
+
+    return(
+        <div>
+            <h1>Pet!</h1>
+            <p>Começando o component de pet</p>
+        </div>
+    )
+}
