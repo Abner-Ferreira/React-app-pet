d3583de5cb1f092eeedc6a7d217ac21c1a1e5208
hitoshidevx <g.hitoshi.yokogawa@gmail.com>
2022-09-30 20:58:32 -0300

    Modelo do component pet finalizado

--- CP2/react-app-pet/src/components/Pet.jsx
+++ CP2/react-app-pet/src/components/Pet.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { FiX as DEL } from 'react-icons/fi'
 
-export default function Pet() {
-
-    return(
+export default function Pet(props) {
+    return (
         <div>
-            <h1>Pet!</h1>
-            <p>Começando o component de pet</p>
+            <div>
+                <h1>{props.pet.titulo}</h1>
+            </div>
+            <img src={props.pet.imagem} alt="imagem do pet"/>
+            <div>
+                <div><p>{props.pet.idade} anos</p></div>
+                <div><p>Da raça {props.pet.raca}</p></div>
+                <div><p>De porte {props.pet.tamanho}</p></div>
+            </div>
+            <br/>
+            <div>
+                <div><p>Dono<br /> {props.pet.dono}</p></div>
+                <div><p>Tel: {props.pet.telefone}</p></div>    
+            </div>   
+            <div>
+                <p>Observação: <br /> {props.pet.observacao}</p>
+            </div>
+            <button onClick={props.remove}><DEL/></button>
         </div>
     )
 }
