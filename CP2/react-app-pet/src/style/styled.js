e1172a3b00f5684ca3d9b2728220da4da5276ea5
BrunaBGM <bruna.gabriela.bgm@gmail.com>
2022-10-01 10:37:46 -0300

    Criacao da estilização dos componentes.

---
+++ CP2/react-app-pet/src/style/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const DivPet = styled.div`
+    background-color : #B8860B;
+    border: solid 3px #333;
+    box-shadow: 5px 5px 5px #333;
+    padding: 10px;
+    align-items: center;
+    text-align: center;
+    width: 400px;
+    height: fit-content;
+    margin: 20px;
+    display: inline-table;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+`
+export const DivCadastro = styled.div`
+    width:auto;
+    background-color:#fff1f3 ;
+    display: flexbox;
+    flex-wrap: wrap;
+`
